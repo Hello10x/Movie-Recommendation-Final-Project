e5487c5e08511418f950a13822fead21a5df526e
Hello10x <117714778+Hello10x@users.noreply.github.com>
2025-12-17 22:39:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,24 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
+# Hàm tải dữ liệu và cache lại để tăng tốc độ
+@st.cache_data
+def load_data():
+    file_path = 'ratings_clean.csv'  # Tên file sẽ lưu trên server
+    
+    # Kiểm tra nếu file chưa tồn tại thì mới tải
+    if not os.path.exists(file_path):
+        # ID file 
+        file_id = '1ZLWgsnkcsJ3ktvtit3t-tS3MXaInuMQC'
+        url = f'https://drive.google.com/uc?id={file_id}'
+        
+        # Tải file về
+        gdown.download(url, file_path, quiet=False)
+    
+    # Đọc file CSV
+    df = pd.read_csv(file_path)
+    return df
+
 # ============================================
 # 1. LOAD DỮ LIỆU & HỆ THỐNG
 # ============================================
@@ -417,3 +435,4 @@ elif st.session_state['page'] == 'viz':
         st.subheader("Phân bố độ phổ biến")
 
         render_chart("popularity_hist.png", "Histogram Popularity")
+
